e2710c8f5ea5048f96c0b6b1ad76f710297257e5
tupaLeha <dusket232888@gmail.com>
2021-09-18 22:42:53 +0300

    dz3

---
+++ dz3.py
@@ -0,0 +1,61 @@
+class Url:
+    def __init__(self, scheme=None, authority=None, path=None, query=None, fragment=None):
+        self.scheme = scheme
+        self.authority = authority
+        self.path = path
+        self.query = query
+        self.fragment = fragment
+
+    def __str__(self):
+        return f"{self.scheme}://{self.authority}/{self.path}/{self.query}/{self.fragment}"
+
+    def __eq__(self, other):
+        return str(self) == str(other)
+
+
+url1 = Url('scheme', 'authority', 'path', 'query', 'fragment')
+url2 = Url('scheme', 'authority', 'path', 'query', 'fragment')
+url3 = Url('scheme1', 'authority2', 'path3', 'query4', 'fragment5')
+print(url1 is str)
+print(url1 == url3)
+print(url1 == url2)
+
+
+class HttpsUrl(Url):
+    def __init__(self, scheme='https', authority=None, path=None, query=None, fragment=None):
+        self.scheme = scheme
+        self.authority = authority
+        self.path = path
+        self.query = query
+        self.fragment = fragment
+
+
+class HttpUrl(Url):
+    def __init__(self, scheme='http', authority=None, path=None, query=None, fragment=None):
+        self.scheme = scheme
+        self.authority = authority
+        self.path = path
+        self.query = query
+        self.fragment = fragment
+
+
+class GoogleUrl(HttpsUrl):
+    def __init__(self, scheme=HttpsUrl().scheme, authority='google.com', path=None, query=None, fragment=None):
+        self.scheme = scheme
+        self.authority = authority
+        self.path = path
+        self.query = query
+        self.fragment = fragment
+
+
+class WikiUrl(HttpsUrl):
+    def __init__(self, scheme=HttpsUrl().scheme, authority='wikipedia.org', path=None, query=None, fragment=None):
+        self.scheme = scheme
+        self.authority = authority
+        self.path = path
+        self.query = query
+        self.fragment = fragment
+
+
+print(WikiUrl())
+print(GoogleUrl())
